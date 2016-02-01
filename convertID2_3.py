596c5965167a783f5761ee30668f4d56a7be1346
GabrielGreslin <gabriel.greslin@gmail.com>
2016-02-01 23:55:44 +0100

    First commit

---
+++ convertID2_3.py
@@ -0,0 +1,24 @@
+'''
+Convert the raw ID2.txt to a csv file
+Remove the boolean value
+'''
+
+file = open("Raw_Data\\ID2.txt","r")
+dest = open("Trans_Data\\ID2_new.txt","w")
+dest.write("latitude,longitude,time\n")
+
+#Number of line to keep
+nbLineTODo = 2000
+i=0
+
+for l in file:
+	if i>nbLineTODo:
+		break
+	i += 1
+	pieces = l.split(" ")
+	newL = "+"+",".join([x for i,x in enumerate(pieces) if i in (0,1,3)])
+	dest.write(newL)
+
+file.close()
+dest.close()
+
